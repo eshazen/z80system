585e9603c1936907c15f65862d4fb0554f7db19f
Eric Hazen <hazen@bu.edu>
2021-12-09 08:23:12 -0500

    tweaking

--- hardware/keyboard/AVR_code/main.c
+++ hardware/keyboard/AVR_code/main.c
@@ -11,26 +11,27 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 // provides: normal[] and shifted[]
 #include "ascii.h"
 
 #define MAXQUE 16
-static unsigned char queue[MAXQUE];
-static unsigned char qh, qt;
+static uint16_t queue[MAXQUE];
+static uint8_t qh, qt;
 
 #define full() (((qt+1)&(MAXQUE-1))==qh)
 #define empty() (qt==qh)
 
-void putq( unsigned char ch) {
+void putq( uint16_t ch) {
   if( !full()) {
     queue[qh] = ch;
     qh = (qh + 1) & (MAXQUE-1);
   }
 }
 
-unsigned char getq() {
-  unsigned char ch = 0;
+uint16_t getq() {
+  uint16_t ch = 0;
   if( !empty()) {
     ch = queue[qt];
     qt = (qt+1) & (MAXQUE-1);
@@ -39,19 +40,24 @@ unsigned char getq() {
 }
 
 // output character to serial port
-void outchr( unsigned char v) {
+void outchr( uint8_t v) {
   while (! (UCSRA & (1 << UDRE)) );
   UDR = v;
 }
 
+void outs( char* s) {
+  while( *s) {
+    outchr( *s);
+    ++s;
+  }
+}
+
 // output two-digit hex value
-void outhex( unsigned char v) {
-  char buff[3];
+void outhex( uint16_t v) {
+  char buff[] = "        ";
 
   itoa( v, buff, 16);
-
-  outchr(buff[0]);
-  outchr(buff[1]);
+  outs( buff);
 }
 
 // output CRLF
@@ -67,13 +73,30 @@ void crlf() {
 #define D_COL_MASK 0x0c
 #define D_LED_MASK 0x80
 
-static unsigned char last_col[8];
+// location of shift/ctrl/alt
+#define SHIFT_L_ROW 3
+#define SHIFT_L_BIT 0
+#define SHIFT_R_ROW 5
+#define SHIFT_R_BIT 6
+#define CTRL_ROW 2
+#define CTRL_BIT 0
+#define ALT_ROW 7
+#define ALT_BIT 0
+
+// bits for shift functions
+#define CTRL 8
+#define SHIFT 9
+#define ALT 10
+#define TEST 11
+
+static uint8_t last_col[8];
 
 // read column and process
-unsigned char read_col( unsigned char row) {
-  static unsigned char code, dcol, i, b;
-  code = 0;
-  unsigned char col = (PINC & C_COL_MASK) | ((PIND & D_COL_MASK) << 4);
+void read_col( uint8_t row) {
+  static uint8_t dcol, i, b;
+  uint16_t code = 0;
+
+  uint8_t col = (PINC & C_COL_MASK) | ((PIND & D_COL_MASK) << 4);
   if( (dcol = (col ^ last_col[row]))) {	/* change in data? */
     if( !(dcol & col)) {
       // process 1's in difference
@@ -85,19 +108,29 @@ unsigned char read_col( unsigned char row) {
     }
     last_col[row] = col;
   }
-  if( code)
+  if( code) {
+  // check shift/ctrl/alt state
+    if( (last_col[SHIFT_L_ROW] & (1<<SHIFT_L_BIT)) ||
+	(last_col[SHIFT_R_ROW] & (1<<SHIFT_R_BIT)))
+      code |= (1 << SHIFT);
+    if( (last_col[CTRL_ROW] & (1<<CTRL_BIT)))
+      code |= (1 << CTRL);
     putq( code);
+  }
+
 }
 
 
-unsigned char col;
-unsigned char rowB, rowD;
+uint8_t col;
+uint8_t rowB, rowD;
 int main(void)
 {
-  unsigned char code;
+  uint16_t code;
+  uint8_t asc;
 
   /* ubrr = F_OSC/(baud*16)-1 */
-  int ubrr = 25;		/* should be 25 for 2400 baud */
+  //  int ubrr = 25;		/* should be 25 for 2400 baud */
+  int ubrr = 12;		/* 4800 baud */
 
   // set rows (and LED) as output
   DDRD = D_ROW_MASK | D_LED_MASK;
@@ -109,8 +142,8 @@ int main(void)
 
   UCSRB = (1 << TXEN);		/* enable USART Tx */
 
-  UBRRL = (unsigned char)(ubrr); /* set baud rate */
-  UBRRH = (unsigned char)(ubrr >> 8);
+  UBRRL = (uint8_t)(ubrr); /* set baud rate */
+  UBRRH = (uint8_t)(ubrr >> 8);
   // set one stop bit, 8 bit characters, no parity, 
   UCSRC = (1 << UCSZ0) | (1 << UCSZ1) | (1 << URSEL);
   
@@ -131,10 +164,21 @@ int main(void)
     PORTB = B_ROW_MASK - (1 << 6);      read_col(6);
     PORTB = B_ROW_MASK - (1 << 7);      read_col(7);
 
-    if( (code = getq())) {
+    if( (code = getq()), (asc = code & 0xff)) {
+
+      if( code & (1<<SHIFT))
+	asc = shifted[code & 0xff];
+      else
+	asc = normal[code & 0xff];
+      
+      if( code & (1<<CTRL))
+	asc &= 223;
       outhex( code);
-      outchr( ' ');
-      outhex( normal[code]);
+      outchr('=');
+      outhex( asc);
+      outchr('"');
+      outchr( asc);
+      outchr('"');
       crlf();
     }
   }
